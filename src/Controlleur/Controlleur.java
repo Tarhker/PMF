d7627d8901a5f8f9e16f8a54b567c4d27ef642e4
Tarhker <gildascousin33@gmail.com>
2017-11-21 17:32:09 +0100

    Soir 21

--- src/Controlleur/Controlleur.java
+++ src/Controlleur/Controlleur.java
@@ -1,5 +1,14 @@
 package Controlleur;
 
+import Modele.Modele;
+
 public class Controlleur {
+	
+	Modele mod;
+
+	public Controlleur(Modele mod) {
+		// TODO Auto-generated constructor stub
+		mod = Modele;
+	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import Controlleur.Controlleur;
+import Modele.Modele;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Modele mod = new Modele();
+		Controlleur cont = new Controlleur(mod);
+		
 	}
 
 }
